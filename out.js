812f72abe19c77fc5937ccce3f2883c4a190e39b
martin.colladop <martincollado1234>
2019-03-15 11:33:23 +0100

    Ultima version

--- out.js
+++ out.js
@@ -1,61 +1,25 @@
 const figlet = require('figlet');
 const chalk = require('chalk');
 
-
-/**
- * Dar color a un string.
- *
- * @param msg    Es string al que hay que dar color.
- * @param color  El color con el que pintar msg.
- * @returns {string} Devuelve el string msg con el color indicado.
- */
 const colorize = (msg, color) => {
-
     if (typeof color !== "undefined") {
         msg = chalk[color].bold(msg);
     }
     return msg;
 };
-
-
-/**
- * Escribe un mensaje de log.
- *
- * @param msg  El String a escribir
- * @param color  Color del texto.
- */
-const log = (msg, color) => {
-
-    console.log(colorize(msg, color));
+const log = (socket, msg, color) => {
+    socket.write(colorize(msg, color) + "\n");
 };
-
-
-/**
- * Escribe un mensaje de log grande.
- *
- * @param msg    Texto a escribir.
- * @param color  Color del texto.
- */
-const biglog = (msg, color) => {
-
-    log(figlet.textSync(msg, { horizontalLayout: 'full' }), color);
+const biglog =(socket, msg, color) => {
+    log(socket, figlet.textSync(msg, { horizontalLayout: 'full'}), color);
 };
-
-
-/**
- * Escribe el mensaje de error emsg.
- *
- * @param emsg Texto del mensaje de error.
- */
-const errorlog = (emsg) => {
-
-    console.log(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}`);
+const errorlog = (socket, emsg) =>{
+    socket.write(`${colorize("Error", "red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright")}\n`);
 };
-
 
 exports = module.exports = {
     colorize,
     log,
     biglog,
     errorlog
-};
+}
